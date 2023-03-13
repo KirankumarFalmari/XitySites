55690ab94b177bef5f1e75a7779de31e85b848b5
Kirankumar Falmari <kirankumar.falmari@profcyma.com>
2023-03-13 19:16:21 +0530

    Mongodb atlas

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
+const data = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 mongoose
-  .connect("mongodb://127.0.0.1:27017/XitySites")
+  .connect(
+    "mongodb+srv://kirankumarfalmari:Kiran123@cluster0.zretqvn.mongodb.net/xitysites?retryWrites=true&w=majority",
+    data
+  )
   .then(() => {
     console.log("connected successfully");
   })
   .catch((error) => {
-    console.log("connection Failed");
+    console.log(error);
   });
+
+// mongodb://127.0.0.1:27017/XitySites

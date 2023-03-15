10b833a38af8f751ccb467b54efb4d40c4d12e5f
Kirankumar Falmari <kirankumar.falmari@profcyma.com>
2023-03-15 12:57:36 +0530

    Error Solve

--- route/directoryadmin.js
+++ route/directoryadmin.js
@@ -6,12 +6,12 @@ const app = express.Router();
 //Define Controller
 const category = require("../Controller/dirctory/CategoryController");
 const subcategory = require("../Controller/dirctory/SubcategoryController");
-// const childcategory = require("../Controller/dirctory/ChildcategoryController");
-// const bussinessuser = require("../Controller/dirctory/Bussines-user/Bussiness-user");
+const childcategory = require("../Controller/dirctory/ChildcategoryController");
+const bussinessuser = require("../Controller/Bussines-user/Bussiness-user");
 
 app.use("/category", auth, category);
 app.use("/subcategory", subcategory);
 app.use("/childcategory", childcategory);
-// app.use("/bussiness-user", bussinessuser);
+app.use("/bussiness-user", bussinessuser);
 
 module.exports = app;
